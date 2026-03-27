a147681026011177506f471c59a45beb6c97848e
sumeetsrivastav07 <sumeetsrivastav0728@gmail.com>
2026-03-27 16:37:47 +0530

    Added 404 route handler

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ app.use("/",taskRoutes);
 app.get("/", (req, res) => {
   res.send("API is running");
 });
+// 404 handler
+app.use((req, res, next) => {
+  res.status(404).json({
+    message: "Route not found"
+  });
+});
+
 app.use(errorHandler);
 app.listen(3000, () => {
   console.log("Server running on port 3000");
