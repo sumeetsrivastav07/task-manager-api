e73e12d7bf5bbdd5878119281a220a3a33dcd740
sumeetsrivastav07 <sumeetsrivastav0728@gmail.com>
2026-03-27 16:14:35 +0530

    Added centralized error handling middleware

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import taskRoutes from "./routes/taskRoutes.js";
 import logger from "./middleware/logger.js";
+import errorHandler from "./middleware/errorHandler.js";
 const app = express();
 app.use(logger);
 app.use(express.json());
@@ -8,7 +9,7 @@ app.use("/",taskRoutes);
 app.get("/", (req, res) => {
   res.send("API is running");
 });
-
+app.use(errorHandler);
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
