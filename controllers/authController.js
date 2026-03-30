db1f473cea5cecd61aef2fbae6b1e890cbdbb5d2
sumeetsrivastav07 <sumeetsrivastav0728@gmail.com>
2026-03-31 01:40:36 +0530

    add JWT token generation on login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,5 @@
+import jwt from "jsonwebtoken";
+const JWT_SECRET = "mysecretkey";
 let users = [];
 
 export const signupUser = (req, res) => {
@@ -71,13 +73,16 @@ export const loginUser = (req, res) => {
     });
   }
 
-  res.status(200).json({
-    success: true,
-    message: "Login successful",
-    data: {
-      id: user.id,
-      name: user.name,
-      email: user.email
-    }
-  });
+  // Generate JWT
+const token = jwt.sign(
+  { userId: user.id, email: user.email },
+  JWT_SECRET,
+  { expiresIn: "1h" }
+);
+
+res.status(200).json({
+  success: true,
+  message: "Login successful",
+  token
+});
 };
