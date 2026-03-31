414b42fd5430de72dc5ddddc07f4960b0fd32128
sumeetsrivastav07 <sumeetsrivastav0728@gmail.com>
2026-03-31 15:47:20 +0530

    protect task routes using JWT middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,3 +1,33 @@
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = "mysecretkey";
+
 export const protect = (req, res, next) => {
-  next();
+  const authHeader = req.headers.authorization;
+
+  // Check if token exists
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({
+      success: false,
+      message: "Authentication required"
+    });
+  }
+
+  // Extract token
+  const token = authHeader.split(" ")[1];
+
+  try {
+    // Verify token
+    const decoded = jwt.verify(token, JWT_SECRET);
+
+    // Attach user info to request
+    req.user = decoded;
+
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      success: false,
+      message: "Invalid or expired token"
+    });
+  }
 };
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -8,12 +8,13 @@ import {
   deleteTask
 } from "../controllers/taskController.js";
 import validateTask from "../middleware/validateTask.js";
+import { protect } from "../middleware/authMiddleware.js";
 const router=express.Router();
 // GET all tasks
-router.get("/tasks",getAllTasks);
+router.get("/tasks",protect,getAllTasks);
 // CREATE task
-router.post("/tasks",validateTask,createTask);
-router.get("/tasks/:id",getTaskById);
-router.put("/tasks/:id",validateTask, updateTask);
-router.delete("/tasks/:id", deleteTask);
+router.post("/tasks",protect,validateTask,createTask);
+router.get("/tasks/:id",protect,getTaskById);
+router.put("/tasks/:id",protect,validateTask, updateTask);
+router.delete("/tasks/:id", protect,deleteTask);
 export default router;
