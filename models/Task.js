bdf55acea1455df4cde739a44df9b328428331a2
sumeetsrivastav07 <sumeetsrivastav0728@gmail.com>
2026-04-13 23:36:51 +0530

    feat: integrate MongoDB with Mongoose, add User and Task models, and connect DB at server startup

---
+++ models/Task.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User", // 🔥 relation
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Task = mongoose.model("Task", taskSchema);
+
+export default Task;
--- server.js
+++ server.js
@@ -1,29 +1,42 @@
 import dotenv from "dotenv";
 dotenv.config();
+
 import express from "express";
 import taskRoutes from "./routes/taskRoutes.js";
 import logger from "./middleware/logger.js";
 import errorHandler from "./middleware/errorHandler.js";
 import authRoutes from "./routes/authRoutes.js";
-
-
+import { connectDB } from "./config/db.js"; 
 const app = express();
+
+// middleware
 app.use(logger);
 app.use(express.json());
-app.use("/",taskRoutes);
+
+// routes
+app.use("/", taskRoutes);
 app.use("/api/auth", authRoutes);
+
 app.get("/", (req, res) => {
   res.send("API is running");
 });
+
 // 404 handler
 app.use((req, res, next) => {
   res.status(404).json({
-    success:false,
-    message: "Route not found"
+    success: false,
+    message: "Route not found",
   });
 });
 
 app.use(errorHandler);
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
+
+const startServer = async () => {
+  await connectDB(); 
+
+  app.listen(3000, () => {
+    console.log("🚀 Server running on port 3000");
+  });
+};
+
+startServer();
